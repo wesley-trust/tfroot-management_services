0e3dd8270021cc68d3f86cd7ced94c32ff343384
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-23 19:52:58 +0100

    Update  [skip ci]

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tfroot-template
-(Description)
-## [CI/CD Pipeline](link_to_pipeline)
+Terraform root configuration for deploying Management Services to Azure, using modules, with Terratest Unit/Integration testing via Go, Regula (OPA) Policy as Code scanning in an Azure DevOps Pipeline
+## [CI/CD Pipeline](https://dev.azure.com/wesleytrust/Terraform/_build?definitionId=66)
 Select a stage to view in Azure DevOps. *Note that 'Skipped' stages in the last run, will show as "unknown" by design.*
 | Pipeline |
 | :------: |
--- locals.tf
+++ locals.tf
@@ -12,10 +12,10 @@ locals {
   resource_recovery_services_instance_count = lookup(lookup(var.resource_recovery_services_instance_count, "${terraform.workspace}", null), var.service_name, null)
 
   # Prod DR Specific
-  resource_bcdr_locations                        = lookup(var.service_location, "BCDR", null)
-  resource_bcdr_instance_size                    = lookup(lookup(var.resource_instance_size, "BCDR", null), var.service_name, null)
-  resource_bcdr_instance_count                   = lookup(lookup(var.resource_instance_count, "BCDR", null), var.service_name, null)
-  resource_bcdr_traffic_manager_location         = lookup(var.service_traffic_manager_location, "BCDR", null)
-  resource_bcdr_recovery_services_locations      = lookup(var.service_recovery_services_location, "BCDR", null)
-  resource_bcdr_recovery_services_instance_count = lookup(lookup(var.resource_recovery_services_instance_count, "BCDR", null), var.service_name, null)
+  resource_bcdr_locations                        = lookup(var.service_location, "${terraform.workspace}_BCDR", null)
+  resource_bcdr_instance_size                    = lookup(lookup(var.resource_instance_size, "${terraform.workspace}_BCDR", null), var.service_name, null)
+  resource_bcdr_instance_count                   = lookup(lookup(var.resource_instance_count, "${terraform.workspace}_BCDR", null), var.service_name, null)
+  resource_bcdr_traffic_manager_location         = lookup(var.service_traffic_manager_location, "${terraform.workspace}_BCDR", null)
+  resource_bcdr_recovery_services_locations      = lookup(var.service_recovery_services_location, "${terraform.workspace}_BCDR", null)
+  resource_bcdr_recovery_services_instance_count = lookup(lookup(var.resource_recovery_services_instance_count, "${terraform.workspace}_BCDR", null), var.service_name, null)
 }
