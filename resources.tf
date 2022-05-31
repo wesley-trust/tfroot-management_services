41e31416615448fa4b9637a5d966bf96b6c06ba7
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-31 19:46:04 +0100

    Update reference [skip ci]

--- resources.tf
+++ resources.tf
@@ -22,13 +22,13 @@ module "management_services_network_peering" {
   service_deployment         = var.service_deployment
   service_name               = var.service_name
   service_location           = each.value
-  resource_network_peer      = module.management_services_virtual_machines[each.value].network_name
-  resource_group_peer        = module.management_services_virtual_machines[each.value].resource_group_name
+  resource_network_peer      = module.management_services[each.value].network_name
+  resource_group_peer        = module.management_services[each.value].resource_group_name
   resource_network_peer_role = var.resource_network_peer_role
 }
 
 module "management_services_traffic_manager" {
-  depends_on                                  = [module.management_services_virtual_machines]
+  depends_on                                  = [module.management_services]
   count                                       = var.provision_traffic_manager == true ? 1 : 0
   source                                      = "github.com/wesley-trust/tfmodule-traffic_manager"
   service_environment                         = terraform.workspace
@@ -36,12 +36,12 @@ module "management_services_traffic_manager" {
   service_name                                = "${var.service_name}-TM"
   service_location                            = local.resource_traffic_manager_location
   resource_name                               = local.resource_name
-  resource_traffic_manager_endpoints          = module.management_services_virtual_machines
+  resource_traffic_manager_endpoints          = module.management_services
   resource_traffic_manager_endpoint_locations = local.resource_locations
 }
 
 module "management_services_recovery_services" {
-  depends_on                                  = [module.management_services_virtual_machines]
+  depends_on                                  = [module.management_services]
   for_each                                    = toset(local.resource_recovery_services_locations)
   source                                      = "github.com/wesley-trust/tfmodule-recovery_services"
   service_environment                         = terraform.workspace
@@ -50,6 +50,6 @@ module "management_services_recovery_services" {
   service_location                            = each.value
   resource_name                               = local.resource_name
   resource_recovery_services_instance_count   = local.resource_recovery_services_instance_count
-  resource_recovery_services_virtual_machines = module.management_services_virtual_machines[each.value]
+  resource_recovery_services_virtual_machines = module.management_services[each.value]
 }
  */
