3614ee9879179956b8c6150e444c0ea0c4c8c571
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-07 16:33:32 +0100

    Second destroy for orphaned resources [skip ci]

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -45,6 +45,9 @@ func TestApplyDefault(t *testing.T) {
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
+	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
+	defer terraform.Destroy(t, terraformOptions)
+
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
