b80f3612f0b7f303e9bb01a3964c760d76a5c70b
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-11 16:15:30 +0100

    Enable delete protection

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -38,7 +38,8 @@ func TestApplyDefault(t *testing.T) {
 
 		// Variables to pass to the Terraform code using -var options
 		Vars: map[string]interface{}{
-			"service_deployment": serviceDeployment,
+			"service_deployment":                 serviceDeployment,
+			"resource_delete_protection_enabled": false,
 		},
 	})
 
