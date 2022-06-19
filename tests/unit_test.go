4309c3eb1dbf1345963e3d47938b7ad72de26391
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-19 14:45:06 +0100

    Udpate module versions

--- tests/unit_test.go
+++ tests/unit_test.go
@@ -35,6 +35,7 @@ func TestPlanDefault(t *testing.T) {
 		// Variables to pass to the Terraform code using -var options
 		Vars: map[string]interface{}{
 			"service_deployment":                 serviceDeployment,
+			"resource_network_peer_deployment":   serviceDeployment,
 			"resource_delete_protection_enabled": false,
 		},
 	})
